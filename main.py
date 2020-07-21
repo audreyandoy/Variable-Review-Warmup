8326460c86e10799d967057203f7e1151664a46f
Audrey Andoy <audreylandoy@gmail.com>
2020-07-21 17:01:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them.
+
+# ○ Use 2 variables in accepting the names (one for the first name and another for the last name).
+
+# first_name = input("What is your first name?")
+# last_name = input("What is your last name?")
+# print("Hello " + last_name + " " + first_name )
+
+# Create a for loop that will increase a counter variable by once
+
+counter = 0
+for i in range(3):
+  counter += 1
+  print(counter)
